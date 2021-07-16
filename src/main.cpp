534a870db7bf328c3e4aff009bdef78d291e0fe0
Tom Hightower <Tom.Hightower356@gmail.com>
2021-07-16 12:21:00 -0400

    deps baby

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "glm/glm.hpp"
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}
