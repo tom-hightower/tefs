bed5ff8cb98bd12b10658475d1dbc2a8a5395dea
Tom Hightower <Tom.Hightower356@gmail.com>
2021-07-20 22:55:14 -0400

    fixed glad, we have window

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@ using namespace glm;
 
 GLFWwindow* window;
 
+const int WIDTH = 960;
+const int HEIGHT = 540;
+
 int main() {
     std::cout << "Hello, world!\n";
     glfwSetErrorCallback(GLFW_Error_Callback);
@@ -16,6 +19,22 @@ int main() {
         fprintf(stderr, "Failed to init GLFW\n");
         return -1;
     }
-    window = glfwCreateWindow(960, 540, "tefs", NULL, NULL);
-    while (true) {}
+    window = glfwCreateWindow(WIDTH, HEIGHT, "tefs", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr, "Failed to create GLFW window.");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        fprintf(stderr, "Failed to initialize GLAD");
+        return -1;
+    }
+    glViewport(0, 0, WIDTH, HEIGHT);
+    
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    }
 }
+ 
